4938d5bed16bbddba6ea81a80211f2fde002b682
jeffcahn <2623768443@qq.com>
2025-07-11 17:03:25 +0800

    add dockerfile

--- src/main/java/com/example/cloudspace/feign/UserFeign.java
+++ src/main/java/com/example/cloudspace/feign/UserFeign.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user-service", url = "http://localhost:8081")
+@FeignClient(name = "user-service", url = "${user.auth:''}")
 public interface UserFeign {
 
 
